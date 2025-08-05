3dbe6967e519e365a1f165a185c2f2c5fafc2e0f
Pakorn Nathong <pakorn.nth@hotmail.com>
2025-08-05 14:59:59 +0700

    feat: enhance Home page with new button components and layout improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+import { Apple } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <main className="container">
-      <h1>Good morning</h1>
+    <main className="container space-y-4 py-8">
+      <h1 className="text-2xl font-bold">
+        Next.js 15 Tailwind v4 Shadcn Template
+      </h1>
+      <div className="flex flex-wrap items-center gap-2">
+        <Button size="lg">Large</Button>
+        <Button size="lg" variant="outline">
+          Large Icon <Apple />
+        </Button>
+        <Button size="default">Default</Button>
+        <Button size="default" variant="outline">
+          Default Icon <Apple />
+        </Button>
+        <Button size="sm">Small</Button>
+        <Button size="sm" variant="outline">
+          Small Icon <Apple />
+        </Button>
+        <Button size="xs">Extra Small</Button>
+        <Button size="xs" variant="outline">
+          Extra Small Icon <Apple />
+        </Button>
+        <Button size="icon">
+          <Apple />
+        </Button>
+        <Button size="iconSm">
+          <Apple />
+        </Button>
+        <Button size="iconXs">
+          <Apple />
+        </Button>
+      </div>
     </main>
   )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -24,8 +24,11 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        xs: "h-6.5 rounded-md gap-1 px-2 text-xs [&_svg:not([class*='size-'])]:size-3.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
+        iconSm: "size-8",
+        iconXs: "size-6.5 [&_svg:not([class*='size-'])]:size-3.5",
       },
     },
     defaultVariants: {
